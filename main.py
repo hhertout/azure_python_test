c8c6d403fd099b21590f88765dae674e44d2dfd9
hhertout <hugues.hertout@gmail.com>
2025-05-04 15:37:07 +0200

    init

---
+++ main.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from fastapi import FastAPI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/health")
+def health_status():
+    return {"up": True}
+
+@app.get("/ready")
+def readiness_status():
+    return {"ready": True}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
